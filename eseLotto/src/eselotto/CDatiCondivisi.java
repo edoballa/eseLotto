e9c75ddc501d1f8b3a048b40e17b0e2452d6294b
Bruno <Bruno@Asus>
2019-03-21 22:08:53 +0100

    Esercizio preverifica

---
+++ eseLotto/src/eselotto/CDatiCondivisi.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eselotto;
+
+/**
+ *
+ * @author ballabio_edoardo
+ */
+public class CDatiCondivisi {
+    private int PrimoNumero;
+    private int SecondoNumero;
+    private boolean PrimoTrovato;
+    private boolean SecondoTrovato;
+    private int[] ruota;
+}
